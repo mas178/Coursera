08dd39f629eb5be86755b2f40ebb11d91fec6b66
mas178 <mas178jp@gmail.com>
2015-12-23 14:09:36 +0900

    modify

--- DevDataProduct/ShinyTest/server.R
+++ DevDataProduct/ShinyTest/server.R
@@ -1,24 +1,19 @@
-library(UsingR)
-data(iris)
+library(UsingR); data(iris)
 
 shinyServer(
   function(input, output) {
-    output$plotIris <- renderPlot({
-      data = subset(iris, Species %in% input$species)
-
-      par(mfrow=c(1,2))
-
-      plot(
-        data$Sepal.Width, data$Sepal.Length,
-        xlim = c(1.8, 4.6), ylim = c(4, 8), col = c("red", "green3", "blue")[unclass(data$Species)],
-        main = "Sepal", xlab = "Width", ylab = "Length")
-      points(mean(data$Sepal.Width), mean(data$Sepal.Length), pch = 4, cex = 5)
+    output$plotIris = renderPlot({
       
-      plot(
-        data$Petal.Width, data$Petal.Length,
-        xlim = c(0, 2.6), ylim = c(0, 7), col = c("red", "green3", "blue")[unclass(data$Species)],
-        main = "Petal", xlab = "Width", ylab = "Length")
-      points(mean(data$Petal.Width), mean(data$Petal.Length), pch = 4, cex = 5)
+      data = subset(iris, Species %in% input$species)
+      
+      irisPlot = function(x, y, xlim, ylim, main) {
+        plot(x, y, col = c("red", "green3", "blue")[unclass(data$Species)], main = main, xlab = "Width", ylab = "Length")
+        points(mean(x), mean(y), pch = 4, cex = 5)
+      }
+      
+      par(mfrow = c(1, 2))
+      irisPlot(data$Sepal.Width, data$Sepal.Length, main = "Sepal")
+      irisPlot(data$Petal.Width, data$Petal.Length, main = "Petal")
     })
   }
 )
--- DevDataProduct/ShinyTest/ui.R
+++ DevDataProduct/ShinyTest/ui.R
@@ -2,12 +2,12 @@ library(shiny)
 shinyUI(fluidPage(
   verticalLayout(
     h1('IRIS Plot'),
-
+    
     h2('ReadMe'),
     p('This is just a sample shiny application. I don\'t mean anything serious. '),
-    p('You can see scatter diagram of iris which you select in "Control".'),
+    p('You can see scatter diagrams of iris which you select in "Control".'),
     p('Black "X" in the diagrams are the mean of the data.'),
-
+    
     h2('Control'),
     checkboxGroupInput(
       "species",
@@ -16,8 +16,9 @@ shinyUI(fluidPage(
       selected = c("setosa", "versicolor", "virginica"),
       inline = F,
       width = "100%"),
-
+    
     h2('Output'),
+    p('Please wait momentarily until the diagrams are displayed.'),
     plotOutput('plotIris')
   )
 ))
